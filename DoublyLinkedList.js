de359d63f5f0136528aaad43750e18654710fa2a
Mehyar <mehyar500@gmail.com>
2021-02-03 09:10:48 -0500

    interview prep

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -6,61 +6,45 @@ var Node = function(data, prev) {
   var DoublyLinkedList = function() {
     this.head = null;
     this.tail = null;
-    
-    //Add method 
-    this.add = (data)=>{
-       if(!data){
-        return false; //return if no data
-      }
-      let newNode = new Node(data, null);
-      if(this.head == null){
-        this.head = newNode;
-      }else{
-        let nextNode = this.head;
-        while(nextNode.next){
-          nextNode = nextNode.next;
+    // change code below this line
+    this.add = function(element) {
+      var node = new Node(element, this.tail);
+      var currentNode = this.head;
+      var previousNode;
+  
+      if (this.head === null) {
+        this.head = node;
+        this.tail = node;
+      } else {
+        while (currentNode.next) {
+          previousNode = currentNode;
+          currentNode = currentNode.next;
         }
-        nextNode.next = newNode;
-      }
-      //create a reference for previous node to the newly added node
-      if(this.tail == null){ 
-        this.tail = newNode;
-      }else{
-        let prevNode = this.tail;
-        this.tail = newNode;
-        this.tail.prev = prevNode
+        node.prev = currentNode;
+        currentNode.next = node;
+        this.tail = node;
       }
     };
-    
-    //Remove method
-    this.remove = (ele)=>{
-      let current = this.head;
-      //if no head and tail => empty
-      if(!this.head && !this.tail){
+    this.reverse = function() {
+      let temp = null;
+      let currentNode = this.head;
+  
+      if (this.head === null) {
         return null;
       }
-      //if element at the end -> remove and update .next to null
-      if(this.tail.data == ele){      
-        let prevNode = this.tail.prev;
-        this.tail = prevNode;
-        this.tail.next = null;
-        return;
+  
+      this.tail = currentNode;
+  
+      while (currentNode) {
+        temp = currentNode.prev;
+        currentNode.prev = currentNode.next;
+        currentNode.next = temp;
+        currentNode = currentNode.prev;
       }
-      //loop over the nodes till the final node, find the node, reference the next node to the previous node and update the node with proper .prev
-      while(current.next){
-        if(current.data == ele){
-          let removedNode = current;
-          let prevNode = current.prev;
-          let nextNode = current.next;
-          if(this.head.data == removedNode.data){
-            this.head = nextNode;
-            this.head.prev = null;
-            break;
-          }
-          prevNode.next = nextNode;
-          nextNode.prev = prevNode;
-        }
-        current = current.next;
+  
+      if (temp != null) {
+        this.head = temp.prev;
       }
-    }
+    };
+    // change code above this line
   };
