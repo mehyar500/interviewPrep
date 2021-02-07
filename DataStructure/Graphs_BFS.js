50863ade919b0d82a8ed62423ed1b195424eb76b
Mehyar <mehyar500@gmail.com>
2021-02-07 15:45:33 -0500

    interview prep

--- DataStructure/Graphs_BFS.js
+++ DataStructure/Graphs_BFS.js
@@ -8,29 +8,46 @@
  * Move to the next layer
  */
 
+ // outputs key value pairs
+ // key is node and value is its distancew from root
 function bfs(graph, root) {
     var nodesLen = {};
     
+    // we'll start all distances at infifinity
     for (var i = 0; i < graph.length; i++) {
       nodesLen[i] = Infinity;
     }
     nodesLen[root] = 0; 
     
+    // we need to create a simple queue to keep track of nodes to visit
     var queue = [root]; 
+    // keep track of current node
     var current; 
   
+    // traversing untill there no nodes to traverse
     while (queue.length != 0) {
+
+      // we'll start the loop by poping off a node from the queue to traverse 
+      // which at the begeinningl, its the root node
       current = queue.shift();
       
+      // we need to get all the nodes connected to the current node
       var curConnected = graph[current];
+      // keep track of list of nodes connected to  current node
       var neighborIdx = []; 
+      // we need to get the first node connected to the current node
+      // because #1 in the array means its connected to that node at that index
       var idx = curConnected.indexOf(1); 
+      // we need a loop to all the nodes connected to current
       while (idx != -1) {
         neighborIdx.push(idx); 
         idx = curConnected.indexOf(1, idx + 1); 
       }
       
+      // loop through connected node and get the distance
       for (var j = 0; j < neighborIdx.length; j++) {
+        // if the value in the nodeLen array at the index of the nieghbor from nieghbor index array == infinity
+        // that means we haven't set the distance of that node yet
         if (nodesLen[neighborIdx[j]] == Infinity) {
           nodesLen[neighborIdx[j]] = nodesLen[current] + 1;
           queue.push(neighborIdx[j]); 
